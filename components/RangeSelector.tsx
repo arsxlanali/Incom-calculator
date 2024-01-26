94c93a1b1e174c5164e91eabcd4e635325bce8c4
arsxlanali <arsxlanali@gmail.com>
2024-01-26 20:08:46 +0500

    Changes for range selector

--- components/RangeSelector.tsx
+++ components/RangeSelector.tsx
@@ -1,35 +1,57 @@
+import { useFormikContext } from "formik";
 import DateSelector from "./DateSelector";
 import { CancelIcon } from "./icons";
-import { memo } from "react";
+import { memo, useState } from "react";
+import { setOptions, useDispatch, useSelector } from "@/lib/redux";
+import { validateDateRanges } from "@/app/utils";
 
 interface Props {
-  number: number;
+  index: number;
+  length: number;
   setCountArray: any;
 }
 
-const RangeSelector: React.FC<Props> = memo(({ number, setCountArray }: Props) => {
-  return (
-    <div className="flex flex-col items-end">
-      <CancelIcon
-        className="pb-1"
-        onClick={() =>
-          setCountArray((prevCountArray: []) => {
-            const newArray = [...prevCountArray];
-            newArray.splice(number, 1);
-            return newArray;
-          })
-        }
-      />
-      <DateSelector
-        fieldMonth={`toMonth${number}`}
-        fieldYear={`toYear${number}`}
-      />
-      <DateSelector
-        fieldMonth={`fromMonth${number}`}
-        fieldYear={`fromYear${number}`}
-      />
-    </div>
-  );
-});
+const RangeSelector: React.FC<Props> = memo(
+  ({ index, length, setCountArray }: Props) => {
+    const formik = useFormikContext<SelectedOptions>();
+    // const [slected, setSelected] = useState()
+    const dispatch = useDispatch();
+    const {
+      [`fromMonth${index}`]: _fromMonth,
+      [`fromYear${index}`]: _fromYear,
+      [`toMonth${index}`]: _toMonth,
+      [`toYear${index}`]: _toYear,
+      ...rest
+    } = formik.values;
+    
+ 
+    return (
+      <div className="flex flex-col items-end">
+        {length === index && index !== 0 && (
+          <CancelIcon
+            className="pb-1"
+            onClick={() => {
+
+              dispatch(setOptions(rest));
+              setCountArray((prevCountArray: []) => [
+                ...Array(prevCountArray.length - 1).keys(),
+              ]);
+            }}
+          />
+        )}
+        <DateSelector
+          fieldMonth={`fromMonth${index}`}
+          fieldYear={`fromYear${index}`}
+          index={index}
+        />
+        <DateSelector
+          fieldMonth={`toMonth${index}`}
+          fieldYear={`toYear${index}`}
+          index={index}
+        />
+      </div>
+    );
+  }
+);
 
 export default RangeSelector;
