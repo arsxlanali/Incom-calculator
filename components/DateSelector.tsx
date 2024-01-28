1dd5e48cb9451169b1bd94fc426de73125c24e61
arsxlanali <arsxlanali@gmail.com>
2024-01-28 16:14:31 +0500

    Changes for date selector

--- components/DateSelector.tsx
+++ components/DateSelector.tsx
@@ -7,15 +7,16 @@ import CustomAutocomplete from "./CustomAutoComplete";
 interface Props {
   fieldMonth: string;
   fieldYear: string;
+  index: number;
 }
 
-export default function DateSelector({fieldMonth, fieldYear}: Props) {
+export default function DateSelector({fieldMonth, fieldYear, index}: Props) {
   const { years, months } = useSelector((state) => state.income);
 
   return (
     <div className="flex w-full flex-wrap md:flex-nowrap gap-4">
-      <CustomAutocomplete label="Select a Year" items={years} field={fieldYear} />
-      <CustomAutocomplete label="Select a Month" items={months} field={fieldMonth} />
+      <CustomAutocomplete label="Select a Year" items={years} field={fieldYear} index={index}/>
+      <CustomAutocomplete label="Select a Month" items={months} field={fieldMonth} index={index}/>
     </div>
   );
 }
