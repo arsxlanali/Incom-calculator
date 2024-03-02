9b3f90db5bbfdad9e225aa07b58b91bb03db86e9
arsxlanali <arsxlanali@gmail.com>
2024-03-02 19:43:41 +0500

    Change the get request to post request

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 // To handle a GET request to /api
-export async function GET(request: Request) {
+export async function POST(request: Request) {
   // Do whatever you want
   return NextResponse.json({ message: "Hello World" }, { status: 200 });
 }
