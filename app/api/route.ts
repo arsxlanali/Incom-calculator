423d169314b5b5451e72570831cb64ddbe42fc81
arsxlanali <arsxlanali@gmail.com>
2024-03-17 14:32:48 +0500

    Add the Fetch APi for exchange rate

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,6 +1,20 @@
 import { NextResponse } from "next/server";
 // To handle a GET request to /api
 export async function POST(request: Request) {
-  // Do whatever you want
-  return NextResponse.json({ message: "Hello World" }, { status: 200 });
+  try {
+    const response = await fetch('https://api.exchangeratesapi.io/v1/latest?access_key=API_KEY&base=USD');
+    
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    
+    const data = await response.json();
+    console.log(data);
+    return NextResponse.json({ message: data }, { status: 200 });
+    // Do something with the fetched data
+  } catch (error) {
+    console.error('There was a problem with your fetch operation:', error);
+    return NextResponse.json({ message: error }, { status: 400 });
+
+  }
 }
