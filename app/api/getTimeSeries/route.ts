2dff28ffc8bd812ce1ec0d20f668db53f57c58e8
arsxlanali <arsxlanali@gmail.com>
2024-03-28 22:51:08 +0500

    Add the Axios and Env for API calls

--- app/api/getTimeSeries/route.ts
+++ app/api/getTimeSeries/route.ts
@@ -1,19 +1,21 @@
 import { NextResponse } from "next/server";
+import axios from "axios";
+
 // To handle a GET request to /api
 export async function GET(request: Request) {
   try {
-    const response = await fetch(`${process.env.BASE_URL}&start_date=2012-05-01&end_date=2012-05-25&symbols=USD,PKR&base=USD`);
+    const response = await axios.get(`${process.env.BASE_URL}&start_date=2012-05-01&end_date=2012-05-25&symbols=USD,PKR&base=USD`);
     
-    if (!response.ok) {
+    if (response.status !== 200) {
       throw new Error('Network response was not ok');
     }
     
-    //const data = await response.json();
-    return NextResponse.json({ message: response }, { status: 200 });
+    //const data = response.data; // If you want to work with the response data
+
+    return NextResponse.json({ message: response.data }, { status: 200 });
     // Do something with the fetched data
   } catch (error) {
     console.error(error);
     return NextResponse.json({ message: error }, { status: 400 });
-
   }
 }
